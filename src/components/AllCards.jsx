48911e20e88e714348edca1dd4ead331f9e59c78
varshalearner <vstcse@gmail.com>
2024-06-15 15:04:51 +0530

    login ui

---
+++ src/components/AllCards.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Cards from './Cards'
+import './CSS/AllCards.css'
+
+const AllCards = () => {
+  return (
+    <div className='AllCards' id='AllCards'>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+    </div>
+  )
+}
+
+export default AllCards
