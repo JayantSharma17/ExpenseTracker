44df29f7e865279f1e4179b552af41a0a7cbbd76
varshalearner <vstcse@gmail.com>
2024-06-29 12:09:50 +0530

    role field added in auth forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,88 @@
-import React, { useState } from 'react'
-import './CSS/Login.css'
-import { Link, useNavigate } from 'react-router-dom'
-import { BallTriangle } from 'react-loader-spinner'
-import axios from 'axios'
-import { BaseURL, message } from '../Global'
-import { useNavbarContext } from '../App'
-
+import React, { useState } from "react";
+import "./CSS/Login.css";
+import { Link, useNavigate } from "react-router-dom";
+import { BallTriangle } from "react-loader-spinner";
+import axios from "axios";
+import { BaseURL, message } from "../Global";
+import { useNavbarContext } from "../App";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loader, setLoader] = useState(false);
-  const { setReloadNavbar,reloadNavbar } = useNavbarContext(); // Access the context
+  const { setReloadNavbar, reloadNavbar } = useNavbarContext();
 
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      
-      console.log(email, password)
+      console.log(email, password);
       if (!email || !password) {
-        message('All input fields are required')
-        return
+        message("All input fields are required");
+        return;
       }
       setLoader(true);
-      const payload = { email, password }
+      const payload = { email, password };
       let res = await axios.post(`${BaseURL}/login`, payload);
-      console.log(res.data.token)
-      console.log(res.data.userId)
-      localStorage.setItem("token",res.data.token)
-      localStorage.setItem("userId",res.data.userId)
+      console.log(res.data.token);
+      console.log(res.data.customerId);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("customerId", res.data.customerId);
       setLoader(false);
-      setReloadNavbar(!reloadNavbar)
+      setReloadNavbar(!reloadNavbar);
 
       // if (res.status === 200) {
-        message('Logged In successfully')
-        navigate('/')
+      message("Logged In successfully");
+      navigate("/");
       // }
-    }
-    catch (error) {
+    } catch (error) {
       setLoader(false);
       if (error.response) {
         if (error.response.status === 400) {
-        message('Invalid login Credentials');
-
+          message("Invalid login Credentials");
         }
       } else {
-        message('Network Error')
-
+        message("Network Error");
       }
-
     }
-  }
+  };
   return (
-    <div className='login'>
+    <div className="login">
+      <div className="loginDiv">
+        <h1>Login Form</h1>
+        <div className="roleDiv" id="roleDiv">
+          <div className="roleLabel" id="roleLabel">
+            {" "}
+            Login as{" "}
+          </div>
+          <div className="roleRadio" id="roleRadio">
+            <div>
+              <input type="radio" id="seller" name="role" value="seller" />
+              <label htmlFor="seller">Seller</label>
+            </div>
+            <div>
+              <input type="radio" id="customer" name="role" value="customer" />
+              <label htmlFor="customer">Customer</label>
+            </div>
+          </div>
+        </div>
+        <input
+          type="text"
+          placeholder="Email Id"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Your Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
 
-      <div className='loginDiv'>
-        <h1>User Login</h1>
-        <input type="text" placeholder='Email Id' value={email} onChange={(e) => { setEmail(e.target.value) }} />
-        <input type="text" placeholder='Your Password' value={password} onChange={(e) => { setPassword(e.target.value) }} />
         <BallTriangle
           height={50}
           width={50}
@@ -69,12 +93,13 @@ const Login = () => {
           wrapperClass=""
           visible={loader}
         />
-        <button onClick={handleLogin}>{' Login '}</button>
-        <p>Don't have an account?<Link to={'/register'}>SignUp</Link></p>
+        <button onClick={handleLogin}>{" Login "}</button>
+        <p>
+          Don't have an account?<Link to={"/register"}>SignUp</Link>
+        </p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
